4efda969367f3de5d1395070da606b2f0e6cfd21
Bùi Minh Thành <thanh.bm@teko.vn>
2019-01-14 16:13:59 +0700

    refactor bowling

--- src/beginner/bowling/frame.ts
+++ src/beginner/bowling/frame.ts
@@ -5,31 +5,39 @@ class Frame {
   score: number;
   bonusScore: number;
   ballsPlayed: number;
+  pinsLeft: number;
   isEnded: boolean;
   hasStrike: boolean;
   hasSpare: boolean;
   maxBalls: number;
+  isExtra: boolean;
 
-  constructor(maxBalls: number = Frame.BALL_COUNT) {
+  constructor(maxBalls: number = Frame.BALL_COUNT, isExtra: boolean = false) {
     this.score = 0;
     this.bonusScore = 0;
     this.ballsPlayed = 0;
     this.isEnded = false;
     this.hasStrike = false;
     this.hasSpare = false;
-    this.maxBalls = maxBalls;
+    this.maxBalls = maxBalls < Frame.BALL_COUNT ? maxBalls : Frame.BALL_COUNT;
+    this.isExtra = isExtra;
+    this.pinsLeft = Frame.MAX_PINS;
   }
 
-  playBall(pinKnocked: number, bonusFactor = 0): void {
-    if (!this.isEnded && pinKnocked <= Frame.MAX_PINS && this.score + pinKnocked <= Frame.MAX_PINS) {
-      this.score += pinKnocked;
-      this.bonusScore += pinKnocked * bonusFactor;
+  playBall(pinsKnocked: number, bonusFactor = 0): boolean {
+    if (!this.isEnded && pinsKnocked <= this.pinsLeft) {
+      this.pinsLeft -= pinsKnocked;
+      this.score += this.isExtra ? 0 : pinsKnocked;
+      this.bonusScore += pinsKnocked * bonusFactor;
 
       this.ballsPlayed++;
-      this.isEnded = this.ballsPlayed === this.maxBalls || (this.ballsPlayed === 1 && pinKnocked === Frame.MAX_PINS);
-      this.hasStrike = pinKnocked === Frame.MAX_PINS;
-      this.hasSpare = this.ballsPlayed === this.maxBalls && this.score === Frame.MAX_PINS;
+      this.isEnded = this.ballsPlayed === this.maxBalls || this.pinsLeft === 0;
+      this.hasStrike = pinsKnocked === Frame.MAX_PINS;
+      this.hasSpare = this.ballsPlayed === this.maxBalls && this.pinsLeft === 0;
+      return true;
     }
+
+    return false;
   }
 
   getScore(): number {
--- src/beginner/bowling/game.ts
+++ src/beginner/bowling/game.ts
@@ -1,7 +1,9 @@
 import Frame from './frame';
 
 class Game {
-  static PLAY_FRAMES_COUNT = 10;
+  static PLAY_FRAMES_COUNT: number = 10;
+  static STRIKE_BALLS_REWARD: number = 2;
+  static SPARE_BALLS_REWARD: number = 1;
 
   frames: Array<Frame>;
   isStarted: boolean;
@@ -14,7 +16,12 @@ class Game {
     this.isStarted = false;
     this.isEnded = false;
     this.extraBallsLeft = 0;
-    this.bonusFactors = [0, 0];
+
+    let maxBonus = Game.STRIKE_BALLS_REWARD > Game.SPARE_BALLS_REWARD ? Game.STRIKE_BALLS_REWARD : Game.SPARE_BALLS_REWARD;
+    this.bonusFactors = new Array<number>(maxBonus);
+    for (let i: number = 0; i < maxBonus; i++) {
+      this.bonusFactors[i] = 0;
+    }
   }
 
   start() {
@@ -32,42 +39,60 @@ class Game {
     if (this.isStarted && this.frames.length > 0) {
       let currentFrame = this.frames[this.frames.length - 1];
       if (currentFrame) {
-        currentFrame.playBall(pinKnocked, this.bonusFactors[0]);
-
-        this.bonusFactors[0] = this.bonusFactors[1];
-        this.bonusFactors[1] = 0;
-
-        if (this.extraBallsLeft > 0) {
-          this.extraBallsLeft--;
-        }
-        if (currentFrame.isEnded) {
-          if (currentFrame.hasStrike) {
-            this.bonusFactors[0] += 1;
-            this.bonusFactors[1] += 1;
-          } else if (currentFrame.hasSpare) {
-            this.bonusFactors[0] += 1;
+        if (currentFrame.playBall(pinKnocked, this.getCurrentBonusFactor())) {
+          this.shiftBonusFactor();
+          if (this.frames.length > Game.PLAY_FRAMES_COUNT && this.extraBallsLeft > 0) {
+            this.extraBallsLeft--;
           }
-          if (this.frames.length < Game.PLAY_FRAMES_COUNT) {
-            this.addFrame(new Frame(2));
-          } else if (this.frames.length === Game.PLAY_FRAMES_COUNT) {
-            if (currentFrame.hasSpare) {
-              this.addFrame(new Frame(1));
-              this.extraBallsLeft = 1;
-              this.bonusFactors = [0, 0];
-            } else if (currentFrame.hasStrike) {
-              this.addFrame(new Frame(2));
-              this.extraBallsLeft = 2;
-              this.bonusFactors = [0, 0];
+
+          if (currentFrame.isEnded) {
+            this.addBonusFactor(currentFrame);
+
+            if (this.frames.length < Game.PLAY_FRAMES_COUNT) {
+              this.addFrame(new Frame());
+            } else if (this.frames.length === Game.PLAY_FRAMES_COUNT) {
+              if (currentFrame.hasSpare) {
+                this.extraBallsLeft = Game.SPARE_BALLS_REWARD;
+                this.addFrame(new Frame(this.extraBallsLeft, true));
+              } else if (currentFrame.hasStrike) {
+                this.extraBallsLeft = Game.STRIKE_BALLS_REWARD;
+                this.addFrame(new Frame(this.extraBallsLeft, true));
+              } else {
+                this.isEnded = true;
+              }
+            } else if (this.frames.length > Game.PLAY_FRAMES_COUNT && this.extraBallsLeft > 0) {
+              this.addFrame(new Frame(this.extraBallsLeft, true));
             } else {
               this.isEnded = true;
             }
-          } else if (this.frames.length > Game.PLAY_FRAMES_COUNT && this.extraBallsLeft > 0) {
-            let ballsCount = this.extraBallsLeft > Frame.BALL_COUNT ? Frame.BALL_COUNT : this.extraBallsLeft;
-            this.addFrame(new Frame(ballsCount));
-          } else {
-            this.isEnded = true;
           }
         }
+      }
+    }
+  }
+
+  getCurrentBonusFactor(): number {
+    return this.bonusFactors[0];
+  }
+
+  shiftBonusFactor(): void {
+    for (let i: number = 0; i < this.bonusFactors.length - 1; i++) {
+      this.bonusFactors[i] = this.bonusFactors[i + 1];
+    }
+    this.bonusFactors[this.bonusFactors.length - 1] = 0;
+  }
+
+  addBonusFactor(currentFrame: Frame): void {
+    if (currentFrame.isEnded && !currentFrame.isExtra) {
+      let rewardBalls = 0;
+      if (currentFrame.hasStrike) {
+        rewardBalls = Game.STRIKE_BALLS_REWARD;
+      } else if (currentFrame.hasSpare) {
+        rewardBalls = Game.SPARE_BALLS_REWARD;
+      }
+
+      for (let i: number = 0; i < rewardBalls; i++) {
+        this.bonusFactors[i] += 1;
       }
     }
   }
